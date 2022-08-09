35c718edf5465aad85408571aa60f512bfada9a3
maheh <maheshmb7712@gmail.com>
2022-08-09 16:23:23 +0530

    started doing exercise

---
+++ 9-8_day2/exercise.js
@@ -0,0 +1,12 @@
+let challenge;
+challenge='30_days_of_javascript'
+
+console.log(challenge);
+
+console.log(challenge.length);
+let challenge1=challenge.toUpperCase();
+console.log(challenge1);
+
+console.log(challenge.substring(2,21));
+
+console.log(challenge.substring(0,2));
