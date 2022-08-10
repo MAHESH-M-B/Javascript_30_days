a1aafa5a6e13d1df55a42a9c0468731936c0bded
maheh <maheshmb7712@gmail.com>
2022-08-10 23:12:08 +0530

    day2 balance

--- 9-8_day2/exercise.js
+++ 9-8_day2/exercise.js
@@ -1,12 +1,24 @@
 let challenge;
 challenge='30_days_of_javascript'
-
 console.log(challenge);
 
 console.log(challenge.length);
 let challenge1=challenge.toUpperCase();
 console.log(challenge1);
-
 console.log(challenge.substring(2,21));
 
 console.log(challenge.substring(0,2));
+
+
+console.log(challenge.includes('script'));
+
+
+console.log(challenge.split( ));
+
+
+let countries='india,australia,unitedkingdom, america';
+
+console.log(countries.split( ','));
+
+
+console.log(challenge.replace('javascript','python'));
