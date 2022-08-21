023d9dbd0bffe9fa20a982671c14618ed517aafd
maheh <maheshmb7712@gmail.com>
2022-08-21 23:34:44 +0530

    day11 exercise

--- exercise/javascript.js
+++ exercise/javascript.js
@@ -3,6 +3,7 @@ console.log(`${input}`);
 
 var input1=prompt('what is the firstnumber');
 var input2=prompt('what is the second number');
-
+input1=parseInt(input1)
+input2=parseInt(input2)
 var output=input1+input2
 console.log(output);
