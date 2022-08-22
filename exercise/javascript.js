48ff6061aa9564775b107e4fb10c161c42c8dfc6
maheh <maheshmb7712@gmail.com>
2022-08-22 23:22:23 +0530

    day 2 at day 12

--- exercise/javascript.js
+++ exercise/javascript.js
@@ -1,9 +1,8 @@
-var input=prompt('what is your name?')
+var input=prompt('what is your name?');
 console.log(`${input}`);
-
 var input1=prompt('what is the firstnumber');
 var input2=prompt('what is the second number');
-input1=parseInt(input1)
-input2=parseInt(input2)
-var output=input1+input2
+input1=parseInt(input1);
+input2=parseInt(input2);
+var output=input1+input2;
 console.log(output);
