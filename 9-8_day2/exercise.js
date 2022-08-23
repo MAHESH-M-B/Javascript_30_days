872d1baf46e8146c95aa28ffa3bcea94bcaacbac
maheh <maheshmb7712@gmail.com>
2022-08-23 23:43:49 +0530

    day 2 at day12

--- 9-8_day2/exercise.js
+++ 9-8_day2/exercise.js
@@ -29,25 +29,25 @@ console.log(challenge.charAt('a'));
 
 console.log(challenge.indexOf('j'));
 
-var name1='mahesh'
+var name1='mahesh';
 
-console.log(`my name is ${name1}`)
+console.log(`my name is ${name1}`);
 var age=22
 
-console.log(`my age is ${age}`)
+console.log(`my age is ${age}`);
 
-let dream='get a job in fang'
+let dream='get a job in fang';
 
-console.log(`my dream is ${dream}`)
+console.log(`my dream is ${dream}`);
 
 console.log(`should i complete my dream ${dream}`);
 console.log(`yes i will fullfil my ${dream}`);
 
-let string1='30 days of javascript'
+let string1='30 days of javascript';
 
 console.log(string1.replace('javascript','python'));
 
-let string2='you cannot use the word because at the end of the sentence because because because is a conjunction'
+let string2='you cannot use the word because at the end of the sentence because because because is a conjunction';
 
 console.log(string2.search('because'));
 
