505912801d6372be405998e18f09806bfc416cdf
maheh <maheshmb7712@gmail.com>
2022-08-25 23:31:20 +0530

    day 2 at day13

--- 9-8_day2/exercise.js
+++ 9-8_day2/exercise.js
@@ -33,22 +33,16 @@ var name1='mahesh';
 
 console.log(`my name is ${name1}`);
 var age=22
-
 console.log(`my age is ${age}`);
 
 let dream='get a job in fang';
-
 console.log(`my dream is ${dream}`);
-
 console.log(`should i complete my dream ${dream}`);
 console.log(`yes i will fullfil my ${dream}`);
 
 let string1='30 days of javascript';
-
 console.log(string1.replace('javascript','python'));
-
 let string2='you cannot use the word because at the end of the sentence because because because is a conjunction';
-
 console.log(string2.search('because'));
 
 
