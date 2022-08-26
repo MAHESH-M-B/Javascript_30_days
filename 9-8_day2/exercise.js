fb989b76750f8ca4a6085a7cc6215b77c6ef05fb
maheh <maheshmb7712@gmail.com>
2022-08-26 23:22:29 +0530

    day 2 at day 14

--- 9-8_day2/exercise.js
+++ 9-8_day2/exercise.js
@@ -6,29 +6,19 @@ console.log(challenge.length);
 let challenge1=challenge.toUpperCase();
 console.log(challenge1);
 console.log(challenge.substring(2,21));
-
 console.log(challenge.substring(0,2));
-
-
 console.log(challenge.includes('script'));
-
-
 console.log(challenge.split( ));
 
 
 let countries='india,australia,unitedkingdom, america';
-
 console.log(countries.split( ','));
-
-
 console.log(challenge.replace('javascript','python'));
 
 
 console.log(challenge.charAt(15));
 console.log(challenge.charAt('a'));
-
 console.log(challenge.indexOf('j'));
-
 var name1='mahesh';
 
 console.log(`my name is ${name1}`);
